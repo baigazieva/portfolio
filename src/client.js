014102a2d2aa09167660fb040489b088f2a1079a
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-22 14:00:13 -0700

    portfolio

--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
-import sanityClient from '@sanity/client'
+
+import sanityClient from '@sanity/client';
 
 export default sanityClient({
-    projectId: "b2k26mp",
-    dataset: "production"
+    projectId: "75olqk0o",
+    dataset: "production",
+    
 })
---
+++ src/components/About.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+	return builder.image(source);
+}
+
+ export default  function About() {
+	const [author, setAuthor] = useState(null);
+
+	useEffect(() => {
+		sanityClient
+			.fetch(
+				`*[_type == 'author']{
+			name,
+			bio,
+			"authorImage": image.asset->url
+		}`
+			)
+			.then((data) => setAuthor(data[0]))
+			.catch(console.error);
+	}, []);
+
+	if (!author) return <div>Loading . . . </div>;
+
+	return (
+		<main className="relative">
+			<img
+				
+				alt="waves crashing against a sandy beach"
+				className="absolute w-screen h-screen"
+			/>
+			<div className="p-10 lg:pt-10 container mx-auto relative">
+				<section className="bg-gray-400 rounded-lg shadow-2xl lg:flex p-10 md:flex p-5">
+					<img
+						src={urlFor(author.authorImage).url()}
+						className="rounded lg:w-48 h-60 mr-8 md:w-12 h-12"
+						alt={author.name}
+					/>
+					<div className="text-lg flex flex-col justify-center">
+						<h1 className="cursive text-6xl text-green-600 mb-4 md: mt-2 float-right">
+							Hi, I'm <span className="text-green-100">{author.name}</span>
+						</h1>
+						<div className="prose lg:prose-xl text-white">
+							<BlockContent
+								blocks={author.bio}
+								projectId="evkrc0h6"
+								dataset="production"
+							/>
+						</div>
+					</div>
+				</section>
+			</div>
+		</main>
+	);
+}
+
